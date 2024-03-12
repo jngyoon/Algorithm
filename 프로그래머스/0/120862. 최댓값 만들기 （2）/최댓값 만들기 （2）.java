fe73970652a1f0aaa45578f594311861c1c1c16e
jngyoon <147566838+jngyoon@users.noreply.github.com>
2024-03-12 23:21:09 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.52 ms, Memory: 72.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] numbers) {
+        int answer = 0;
+        
+        Arrays.sort(numbers);
+        int n = numbers.length;
+        
+        //처음 풀었던 방식 -> 음수값이 해결 안됐음
+        // for(int i=0; i<numbers.length; i++){
+        //     if(numbers[i]<0){
+        //         answer = numbers[0]*numbers[1];
+        //     }else{
+        //         answer = numbers[n-1]*numbers[n-2];
+        //     }    
+        // }
+        
+         
+        // 가장 작은 두 수와 가장 큰 두 수의 곱 중 큰 값을 선택합니다.
+        answer = Math.max(numbers[0] * numbers[1], numbers[n-1] * numbers[n-2]);
+        
+        
+        return answer;
+    }
+}
