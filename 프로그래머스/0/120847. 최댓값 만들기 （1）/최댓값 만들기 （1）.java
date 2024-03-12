4d1496d2a910f110283d5ecf9efd9d82a9eafbcf
jngyoon <147566838+jngyoon@users.noreply.github.com>
2024-03-12 18:40:36 +0900

    [level 0] Title: 최댓값 만들기 (1), Time: 0.35 ms, Memory: 81.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] numbers) {
+        int answer = 0;
+        
+        Arrays.sort(numbers);
+        
+        int n = numbers.length;
+        
+        answer = numbers[n-1]*numbers[n-2];
+        
+       
+        
+        return answer;
+    }
+}
