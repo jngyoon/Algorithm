a75092efdd4a4abc97dd844e5649e23024134f89
jngyoon <147566838+jngyoon@users.noreply.github.com>
2024-04-09 15:04:23 +0900

    [level 0] Title: 개미 군단, Time: 0.02 ms, Memory: 72.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120837. 개미 군단/개미 군단.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int hp) {
+        int answer = 0;
+        
+        
+        //장군 : 5, 병정 : 3, 일 : 1
+        answer = hp/5+(hp%5)/3+(hp%5)%3;
+        
+        return answer;
+    }
+}
