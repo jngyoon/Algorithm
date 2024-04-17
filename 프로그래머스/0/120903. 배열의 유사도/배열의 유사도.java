1820390fef26685ebe35992bd2a5551b1a30de7e
jngyoon <147566838+jngyoon@users.noreply.github.com>
2024-04-17 18:37:39 +0900

    [level 0] Title: 배열의 유사도, Time: 0.03 ms, Memory: 80.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(String[] s1, String[] s2) {
+        
+        int count = 0;
+        
+        for(String str1 : s1){
+            for(String str2 : s2){
+                if(str1.equals(str2)){
+                    count++;
+                }
+            }
+        }
+        
+        return count;
+    }
+}
