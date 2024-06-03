fd5f0b23b7b1c6ceae05f58b13322a3e99c2693b
jngyoon <147566838+jngyoon@users.noreply.github.com>
2024-06-03 15:27:11 +0900

    [level 0] Title: 문자열 계산하기, Time: 0.13 ms, Memory: 78.9 MB -BaekjoonHub

--- 프로그래머스/0/120902. 문자열 계산하기/문자열 계산하기.java
+++ 프로그래머스/0/120902. 문자열 계산하기/문자열 계산하기.java
@@ -1,9 +1,13 @@
 class Solution {
     public int solution(String my_string) {
+        
+        //공백을 기준으로 문자열 split한 후 배열 생성
         String[] arr = my_string.split(" ");
         
+        //arr[0]의 문자열을 정수로 변환
         int answer = Integer.valueOf(arr[0]);
         
+        //arr[1]이 +인지 -인지에 따라 arr[2]를 정수로 변환한 값을 더하거나 빼기
         for(int i=1; i<arr.length; i++){
             if(arr[i].equals("+")){
                 answer += Integer.valueOf(arr[i+1]);
